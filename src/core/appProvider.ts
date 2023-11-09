95b1338cf2fbcf3555ff6fd646110b1ddcdec3a1
Ioannis Tourkogiorgis <jtourkos@gmail.com>
2023-11-09 17:55:10 +0100

    refactor: change the provider's polling interval

--- src/core/appProvider.ts
+++ src/core/appProvider.ts
@@ -2,7 +2,7 @@ import { JsonRpcProvider } from 'ethers';
 import appSettings from '../config/appSettings';
 
 const appProvider = new JsonRpcProvider(appSettings.rpcUrl, undefined, {
-  pollingInterval: 1000,
+  pollingInterval: 5000,
 });
 
 export default appProvider;
