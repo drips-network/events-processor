d30d8a765e282cfb57d43d4771e1c0ff112cc6c9
Ioannis Tourkogiorgis <jtourkos@gmail.com>
2024-04-29 17:14:56 +0200

    chore: update .env template

--- src/config/appSettings.ts
+++ src/config/appSettings.ts
@@ -4,21 +4,30 @@ import shouldNeverHappen from '../utils/shouldNeverHappen';
 dotenv.config({ path: `.env.${process.env.ENV}` });
 
 const appSettings = {
-  rpcUrl: process.env.RPC_URL || shouldNeverHappen('RPC_URL is not set.'),
-  logLevel: process.env.LOG_LEVEL,
+  network: process.env.NETWORK || shouldNeverHappen('NETWORK is not set.'),
+  rpcUrl: (() => {
+    const rpcUrl =
+      process.env.RPC_URL || shouldNeverHappen('RPC_URL is not set.');
+
+    if (!rpcUrl.includes(process.env.NETWORK!)) {
+      throw new Error(
+        `RPC_URL '${rpcUrl}' does not match NETWORK '${process.env.NETWORK}'.`,
+      );
+    }
+
+    return rpcUrl;
+  })(),
+  logLevel: process.env.LOG_LEVEL || 'debug',
+  pollingInterval: Number(process.env.POLLING_INTERVAL) ?? 5000,
+  ipfsGatewayUrl:
+    process.env.IPFS_GATEWAY_URL || 'https://drips.mypinata.cloud',
   queueUiPort: process.env.MONITORING_UI_PORT ?? 3000,
-  pinataSdkKey: process.env.PINATA_SDK_KEY,
-  ipfsGatewayUrl: process.env.IPFS_GATEWAY_URL,
-  pinataSdkSecret: process.env.PINATA_SDK_SECRET,
-  network: process.env.NETWORK,
   redisConnectionString: process.env.REDIS_CONNECTION_STRING,
   postgresConnectionString: process.env.POSTGRES_CONNECTION_STRING,
-  environment: process.env.ENV ?? 'local',
-  shouldProcessPastEvents:
-    (process.env.SHOULD_PROCESS_PAST_EVENTS as unknown as string) === 'true',
   shouldStartMonitoringUI:
     (process.env.SHOULD_START_MONITORING_UI as unknown as string) === 'true',
-  pollingInterval: Number(process.env.POLLING_INTERVAL) ?? 5000,
+  shouldProcessPastEvents:
+    (process.env.SHOULD_PROCESS_PAST_EVENTS as unknown as string) === 'true',
 } as const;
 
 export default appSettings;
--- src/core/environment.d.ts
+++ src/core/environment.d.ts
@@ -3,20 +3,18 @@ import type { SupportedNetwork } from './types';
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      RPC_URL: string;
-      LOG_LEVEL: 'debug' | 'info' | 'warn' | 'error';
-      MONITORING_UI_PORT: number;
-      INFURA_API_KEY: string;
-      PINATA_SDK_KEY: string;
-      IPFS_GATEWAY_URL: string;
-      PINATA_SDK_SECRET: string;
       NETWORK: SupportedNetwork;
+      RPC_URL: string;
+      NODE_ENV: 'development' | 'production';
+      LOG_LEVEL: 'debug' | 'info' | 'warn' | 'error';
+      POLLING_INTERVAL: number;
+      IPFS_GATEWAY_URL: string;
+      MONITORING_UI_PORT: number;
       REDIS_CONNECTION_STRING: string;
       POSTGRES_CONNECTION_STRING: string;
-      ENV: 'local' | SupportedNetwork;
       SHOULD_START_MONITORING_UI: boolean;
       SHOULD_PROCESS_PAST_EVENTS: boolean;
-      POLLING_INTERVAL: number;
+      ENV: 'local' | SupportedNetwork;
     }
   }
 }
