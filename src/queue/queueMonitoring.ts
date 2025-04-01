3f20c88fc6be85978d184e836da7c95397314d23
Georgios Jason Efstathiou <j@jason-e.dev>
2025-04-01 14:12:20 +0200

    remove leftover fn

--- src/queue/queueMonitoring.ts
+++ src/queue/queueMonitoring.ts
@@ -1,8 +1,6 @@
-import express from 'express';
 import Arena from 'bull-arena';
 import BeeQueue from 'bee-queue';
 import appSettings from '../config/appSettings';
-import logger from '../core/logger';
 
 export const arenaConfig = Arena(
   {
@@ -23,15 +21,3 @@ export const arenaConfig = Arena(
     disableListen: true,
   },
 );
-
-export default function startQueueMonitoringUI() {
-  const app = express();
-
-  app.use('/arena', arenaConfig);
-
-  const port = appSettings.queueUiPort;
-
-  app.listen(port, () => {
-    logger.info(`Monitor Drips Queues on http://localhost:${port}/arena`);
-  });
-}
