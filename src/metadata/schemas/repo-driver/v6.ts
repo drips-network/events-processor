ffb2c60b620bbac8d350e9fc57b3cc02ef844d92
Morgan Brown <git@email.mhgbrown.is>
2025-10-20 16:11:57 +0200

    Add repoDriverAccountMetadataSchemaV6 to support orcid repo driver split

--- src/metadata/schemas/repo-driver/v6.ts
+++ src/metadata/schemas/repo-driver/v6.ts
@@ -1,4 +1,10 @@
 import z from 'zod';
+import { repoDriverAccountMetadataSchemaV5 } from './v5';
+import {
+  addressDriverSplitReceiverSchema,
+  repoDriverSplitReceiverSchema,
+} from './v2';
+import { dripListSplitReceiverSchema } from './v3';
 
 export const orcidSplitReceiverSchema = z.object({
   type: z.literal('orcid'),
@@ -7,6 +13,19 @@ export const orcidSplitReceiverSchema = z.object({
   orcidId: z.string(),
 });
 
-// TODO: actually export new version
-// should allow orcidSplitReceiverSchema as a dependency
-// for repoDriverAccountSplitsSchema
+const repoDriverAccountSplitsSchema = z.object({
+  maintainers: z.array(addressDriverSplitReceiverSchema),
+  dependencies: z.array(
+    z.union([
+      dripListSplitReceiverSchema,
+      repoDriverSplitReceiverSchema,
+      addressDriverSplitReceiverSchema,
+      orcidSplitReceiverSchema,
+    ]),
+  ),
+});
+
+export const repoDriverAccountMetadataSchemaV6 =
+  repoDriverAccountMetadataSchemaV5.extend({
+    splits: repoDriverAccountSplitsSchema,
+  });
